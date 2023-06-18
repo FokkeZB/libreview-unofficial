2fcd18ee9decbb557bad90a2e5bb20860fe2d104
Fokke Zandbergen <mail@fokkezb.nl>
2023-06-18 20:09:08 +0200

    First version

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNjQ2Ng/spectral.js?branch_slug=master&token=3b846d88-6d39-469e-b1be-f01c992648e6";
+export default { extends: ruleset };
